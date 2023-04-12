cc40de9f62a578b15aa628afb178ee46aa78d405
karikey rana <rana.kartikey999@gmail.com>
2023-04-12 11:31:19 +0530

    before food app build

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const parent1 = React.createElement("h1", {id: 'hi'}, "children")
 root.render(parent1);
