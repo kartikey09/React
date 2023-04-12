ab69501f115a1767caa02f84fd6183aac81cabe7
karikey rana <rana.kartikey999@gmail.com>
2023-04-12 11:35:12 +0530

    first commit for this branch

--- app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const parent1 = React.createElement("h1", {id: 'hi'}, "children")
-root.render(parent1);
